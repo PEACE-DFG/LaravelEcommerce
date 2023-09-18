dcd79099bea6c34d3f4da4575e27793ab3c9d87c
PEACE-DFG <awofesobipeace@gmail.com>
2023-09-18 21:09:39 +0100

    Home Page Done

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,50 @@
+ <!-- footer section start -->
+ <div class="footer_section layout_padding">
+    <div class="container">
+       <div class="footer_logo"><a href="index.html"><img src="images/footer-logo.png"></a></div>
+       <div class="input_bt">
+          <input type="text" class="mail_bt" placeholder="Your Email" name="Your Email">
+          <span class="subscribe_bt" id="basic-addon2"><a href="#">Subscribe</a></span>
+       </div>
+       <div class="footer_menu">
+          <ul>
+             <li><a href="#">Best Sellers</a></li>
+             <li><a href="#">Gift Ideas</a></li>
+             <li><a href="#">New Releases</a></li>
+             <li><a href="#">Today's Deals</a></li>
+             <li><a href="#">Customer Service</a></li>
+          </ul>
+       </div>
+       <div class="location_main">Help Line  Number : <a href="#">+234 811 640 5518</a></div>
+    </div>
+ </div>
+ <!-- footer section end -->
+ <!-- copyright section start -->
+ <div class="copyright_section">
+    <div class="container">
+       <p class="copyright_text">© 2022 All Rights Reserved. Remake by CODEMaster</a></p>
+    </div>
+ </div>
+ <!-- copyright section end -->
+ <!-- Javascript files-->
+ <script src="{{ asset('js/jquery.min.js') }}"></script>
+ <script src="{{ asset('js/popper.min.js') }}"></script>
+ <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+ {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
+ <script src="{{ asset('js/jquery-3.0.0.min.js') }}"></script>
+ <script src="{{ asset('js/plugin.js') }}"></script>
+ <!-- sidebar -->
+ <script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+ <script src="{{ asset('js/custom.js') }}"></script>
+ <script>
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+ </script>
+</body>
+</html>
